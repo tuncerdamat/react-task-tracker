9fa17e2499a4f2902301f5c24e935c7445735539
tuncerdamat <tuncer.damat@gmail.com>
2022-10-20 22:44:59 +0200

    Toggle Reminder

--- src/App.js
+++ src/App.js
@@ -24,15 +24,21 @@ function App() {
         },
     ])
     
+    // Delete Task
     const deleteTask = (id) => {
         setTasks(tasks.filter((task) => task.id !== id ))
+    }
+    
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+        setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
     }
     
   return (
     <div className="container">
       <Header></Header>
         {
-            tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}></Tasks>
+            tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}></Tasks>
             : 'No tasks to show'
         }
     </div>
